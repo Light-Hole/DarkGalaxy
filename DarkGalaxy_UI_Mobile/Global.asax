e42b06d007807011a16990fec98b6dae687f53e7
张耀文 <1650860607@qq.com>
2018-03-11 06:21:57 +0800

    版.本：v1.2.0_dev 添加移动端UI项目

---
+++ DarkGalaxy_UI_Mobile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DarkGalaxy_UI_Mobile.MvcApplication" Language="C#" %>
---
+++ DarkGalaxy_UI_Mobile/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using DarkGalaxy_Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DarkGalaxy_UI_Mobile
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //注册路由规则
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            //启用数据库依赖
+            Helper_Cache.EnableSqlCacheDependency();
+        }
+    }
+}
